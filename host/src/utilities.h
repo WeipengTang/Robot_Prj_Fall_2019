d3ea37ba74f0e868f4e568e7ccf56fb3e0557092
root <samwisetang@hotmail.com>
2019-10-24 16:42:39 -0400

    milestone for wireless control

--- host/src/utilities.h
+++ host/src/utilities.h
@@ -33,4 +33,5 @@ typedef struct robot_control Robot_control;
 void data_32bit_convertor(unsigned char *instruction, int value);
 unsigned int mapValue(unsigned int minIn, unsigned int maxIn, unsigned int minOut, unsigned int maxOut, unsigned value);
 unsigned int abs_value(int value);
+int msleep(long msec);
 #endif
